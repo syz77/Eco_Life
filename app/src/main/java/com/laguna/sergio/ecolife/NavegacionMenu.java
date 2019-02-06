42c8a554776e959359f3d6daf54d21c65cfd0f17
syz77 <sergiolagunac92@gmail.com>
2019-02-06 09:25:01 -0400

    creados: talonariocard.xml usuarioscard.xml DataAdapterGesU.java DataAdapterTalo.java RecyclerAdapGesU.java RecyclerAdapTalo.java
    agregados:
    colores
    string
    imagenes en la carpeta drawable
    
    modificado:
    activity_gestionar_user.xml
    activity_talonarios.xml
    GestionarUser.java
    Talonarios.java
    AndroidManifest (cambios en pantallas)
    build.gradle (soporte para recyclerview)
    
    cambios de la interfaz, agregado algunas imagenes.

--- app/src/main/java/com/laguna/sergio/ecolife/NavegacionMenu.java
+++ app/src/main/java/com/laguna/sergio/ecolife/NavegacionMenu.java
@@ -1,13 +1,20 @@
 package com.laguna.sergio.ecolife;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.view.KeyEvent;
@@ -20,9 +27,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static android.view.View.VISIBLE;
@@ -41,7 +56,17 @@ public class NavegacionMenu extends AppCompatActivity
     RecyclerView recyclerViewG;
     RecyclerView.Adapter recyclerViewadapterG;
     RecyclerView.LayoutManager recyclerViewlayoutManagerG;
+    ImageView ImgEditUser;
 
+    /////////////////////////Para ventas al credito/////////////////////////////////////////////////
+    ImageView btn_hacerfoto,img;
+    ////////////////////////Para ventas al contado//////////////////////////////////////////////////
+    Spinner SpinnerVcont;
+    TextView Vcontfecha,Vcfecha;
+    String vcontprod;
+    /////////////////////Para seleccionar la lista de gestion de usuario///////////////////////////
+    View ChildViewG ;
+    Button RegUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,10 +74,40 @@ public class NavegacionMenu extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         VentaC= (FrameLayout) findViewById(R.id.activity_venta_credito);
         Inicio= (FrameLayout) findViewById(R.id.inicio);
-        Ventas= (FrameLayout) findViewById(R.id.activity_ventas);
+        Ventas= (FrameLayout) findViewById(R.id.activity_ventas_contado);
         Historial= (FrameLayout) findViewById(R.id.activity_talonarios);
         GesUsuario= (FrameLayout) findViewById(R.id.activity_gestionar_user);
         setSupportActionBar(toolbar);
+
+        ///////////////////////////////Para ventas al contado y credito/////////////////////////////////////
+        Vcontfecha=findViewById(R.id.vcontfecha);
+        Vcfecha=findViewById(R.id.vcfecha);
+        SpinnerVcont= findViewById(R.id.spinnervcont);
+
+        String[] pokemon = {"MEGA JUNIOR","NONI ENERGY","FIBRA PLUS","MEGA FAMILY"};
+        SpinnerVcont.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, pokemon));
+        SpinnerVcont.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                //Toast.makeText(adapterView.getContext(), (String) adapterView.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                vcontprod= adapterView.getItemAtPosition(position).toString();//adapterView.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                // vacio
+
+            }
+        });
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        RegUser= findViewById(R.id.button_reg_user);
+        ImgEditUser= findViewById(R.id.editarGU);
+        btn_hacerfoto = findViewById(R.id.vccamara);
+        img = findViewById(R.id.img);
 
         DataAdapterClassListT = new ArrayList<>();
         DataAdapterClassListT.clear();
@@ -69,16 +124,78 @@ public class NavegacionMenu extends AppCompatActivity
         recyclerViewG.setLayoutManager(recyclerViewlayoutManagerG);
 
 
+        btn_hacerfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creamos el Intent para llamar a la Camara
+                Intent cameraIntent = new Intent(
+                        android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                //Creamos una carpeta en la memeria del terminal
+                File imagesFolder = new File(
+                        Environment.getDataDirectory(),"AndroidFacil");
+                imagesFolder.mkdirs();
+                //añadimos el nombre de la imagen
+                File image = new File(imagesFolder, "foto.jpg");
+                Uri uriSavedImage = Uri.fromFile(image);
+                //Le decimos al Intent que queremos grabar la imagen
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+                //Lanzamos la aplicacion de la camara con retorno (forResult)
+                startActivityForResult(cameraIntent, 1);
+            }});
+
+        recyclerViewG.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+
+            GestureDetector gestureDetectorG = new GestureDetector(NavegacionMenu.this, new GestureDetector.SimpleOnGestureListener() {
+
+                @Override public boolean onSingleTapUp(MotionEvent motionEvent) {
+
+                    return true;
+                }
+                //@Override public boolean onLongPress(MotionEvent e) {
+
+                //    return true;
+
+                //}
+
+            });
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
+
+                ChildViewG = Recyclerview.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+
+
+                if(ChildViewG != null && gestureDetectorG.onTouchEvent(motionEvent)) {
+                    Intent intent = new Intent(NavegacionMenu.this, RegUser.class);
+                    startActivity(intent);
+                }
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
+
+                ChildViewG = Recyclerview.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+
+                if (ChildViewG != null && ImgEditUser.onTouchEvent(motionEvent)){
+                    Intent intent = new Intent(NavegacionMenu.this, EditarUser.class);
+                    startActivity(intent);
+
+                }
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Inicio.setVisibility(View.INVISIBLE);
-                Ventas.setVisibility(View.INVISIBLE);
-                Historial.setVisibility(View.INVISIBLE);
-                GesUsuario.setVisibility(View.INVISIBLE);
-                VentaC.setVisibility(View.VISIBLE);
+
                 /*
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
@@ -138,10 +255,13 @@ public class NavegacionMenu extends AppCompatActivity
         Historial.setVisibility(View.INVISIBLE);
         GesUsuario.setVisibility(View.INVISIBLE);
         if (id == R.id.nav_camera) {
+            String Sfecha = getCurrentTimeStamp();
+            Vcontfecha.setText(Sfecha);
             Inicio.setVisibility(View.VISIBLE);
 
+
         } else if (id == R.id.nav_gallery) {
-            Ventas.setVisibility(View.VISIBLE);
+
 
         } else if (id == R.id.nav_slideshow) {
             generarHistorialTalo();
@@ -168,7 +288,7 @@ public class NavegacionMenu extends AppCompatActivity
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if ((keyCode == KeyEvent.KEYCODE_BACK)) {
-            //Acción
+
         }
         return false;
     }
@@ -191,4 +311,65 @@ public class NavegacionMenu extends AppCompatActivity
         recyclerViewadapterG = new RecyclerAdapGesU(DataAdapterClassListG, this);
         recyclerViewG.setAdapter(recyclerViewadapterG);
     }
+
+    public void registrarUsuario(View v){
+        Intent intent = new Intent(NavegacionMenu.this, RegUser.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.left_in, R.anim.left_out);
+    }
+
+    public void editarUsuario(View v){
+        Intent intent = new Intent(NavegacionMenu.this, EditarUser.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.left_in, R.anim.left_out);
+    }
+
+    //////////////////////////////Para sacar fotos////////////////////////////////////////////
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Comprovamos que la foto se a realizado
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            //Creamos un bitmap con la imagen recientemente
+            //almacenada en la memoria
+            Bitmap bMap = BitmapFactory.decodeFile(
+                    Environment.getExternalStorageDirectory()+
+                    "/AndroidFacil/"+"foto.jpg");
+            //Añadimos el bitmap al imageView para
+            //mostrarlo por pantalla
+            img.setImageBitmap(bMap);
+        }
+    }
+
+    public static String getCurrentTimeStamp(){
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            //SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentTimeStamp = dateFormat.format(new Date()); // Find todays date
+
+            return currentTimeStamp;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void ventCont(View v){
+        String Sfecha = getCurrentTimeStamp();
+        Vcontfecha.setText(Sfecha);
+        VentaC.setVisibility(View.INVISIBLE);
+        Inicio.setVisibility(View.INVISIBLE);
+        Ventas.setVisibility(View.VISIBLE);
+    }
+
+    public void ventCred(View v){
+        String Sfecha = getCurrentTimeStamp();
+        Vcfecha.setText(Sfecha);
+        Ventas.setVisibility(View.INVISIBLE);
+        Inicio.setVisibility(View.INVISIBLE);
+        VentaC.setVisibility(View.VISIBLE);
+    }
+
+    public void cadena(){
+
+    }
+
 }
