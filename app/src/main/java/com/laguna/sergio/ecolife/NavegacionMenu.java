d43ce7d4608881629ea183084fa1affd9599ba2b
syz77 <sergiolagunac92@gmail.com>
2019-02-06 10:42:57 -0400

    creados: talonariocard.xml usuarioscard.xml DataAdapterGesU.java DataAdapterTalo.java RecyclerAdapGesU.java RecyclerAdapTalo.java
    agregados:
    colores
    string
    imagenes en la carpeta drawable
    
    modificado:
    activity_gestionar_user.xml
    activity_talonarios.xml
    GestionarUser.java
    Talonarios.java
    AndroidManifest (cambios en pantallas)
    build.gradle (soporte para recyclerview)
    
    cambios de la interfaz, agregado algunas imagenes.
    probando

--- app/src/main/java/com/laguna/sergio/ecolife/NavegacionMenu.java
+++ app/src/main/java/com/laguna/sergio/ecolife/NavegacionMenu.java
@@ -344,7 +344,7 @@ public class NavegacionMenu extends AppCompatActivity
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
             //SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             String currentTimeStamp = dateFormat.format(new Date()); // Find todays date
-            //para la fecha
+            //para la fecha parpara
 
             return currentTimeStamp;
         } catch (Exception e) {
